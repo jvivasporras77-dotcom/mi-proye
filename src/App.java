f08aaaeed61cfe9420f88463e7ec3763dfc2e4b1
jonathannnnn <jvivasporras77@gmail.com>
2026-08-18 19:37:50 -0500

    subir la clase 18 08

---
+++ src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    //esta es la entrada a mi programa 
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        int a = 10, b=3;
+        System.out.println( a + b);
+        System.out.println( a - b);
+        System.out.println( a * b);
+        System.out.println( a / b);
+        System.out.println( a % b);
+        
+        
+    }
+}
+// clases pascal case Mi clase primero en mayuscula 
+//Variable/metodo camel case primera en minuscula y luego en mayucula  
+//constante sigue siendo una variable y para saber que es una constante vamos en mayuscula 
---
+++ src/MiClase.java
@@ -0,0 +1,29 @@
+public class MiClase {
+    //tipos de datos primitivos 
+    private int edad=19;
+    private double altura=1.78;
+    private char sexo ='M'; // siempre se usan comillas simples 
+    private boolean aprobado= false;
+    private String nombre = "jonathan vivas";
+    private String saludo = "Hola," + nombre; 
+    private double nota;
+    nota=10;
+    private String ciudad = "Bogota"
+    private double precio = 9.99;
+    private int precioCortado = (int) precio;
+    private int entero = 5;
+    private double decimal = entero;
+    
+
+
+
+
+
+
+ 
+
+
+
+  
+    
+}
