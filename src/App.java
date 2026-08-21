57833dca45f1556af1dd01370b91316240d5f5d9
jonathannnnn <jvivasporras77@gmail.com>
2026-08-20 19:27:13 -0500

    hola

--- src/App.java
+++ src/App.java
@@ -1,17 +1,92 @@
+import java.util.Scanner;
+
 public class App {
     //esta es la entrada a mi programa 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception { 
+        System.out.println("buenas noches");
         int a = 10, b=3;
         System.out.println( a + b);
         System.out.println( a - b);
         System.out.println( a * b);
         System.out.println( a / b);
         System.out.println( a % b);
+
+        // operadores logicos
+
+        boolean mayorDeEdad = true;
+        boolean tienenCarnet = false;
+
+        System.out.println(mayorDeEdad && tienenCarnet ); // false - ambos deben ser true
+        System.out.println(mayorDeEdad || tienenCarnet ); // true - al menos uno true
+        System.out.println(!mayorDeEdad ); // false - niega el valor 
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("cual es tu base?");
+        int base = sc.nextInt();
+
+        System.out.print("cual es tu altura?");
+        int altura = sc.nextInt();
         
+        System.out.println("el area del triangulo es: " + base  * altura  );
+
+
+        System.out.print ("Nota 1: ");
+        double nota1 = sc.nextDouble();
+
+        System.out.print ("Nota 2: ");
+        double nota2 = sc.nextDouble();
+
+        System.out.print ("Nota 33: ");
+        double nota3 = sc.nextDouble();
+
+
+
+        double promedio = (nota1 + nota2 + nota3) / 3;
+
+        System.out.println("tu promedio es : " + promedio );
         
+
+        System.out.print("cual es tu nota " );
+        int nota = sc.nextInt();
+
+
+
+        if ( nota >= 90 ) {
+        System.out.println("aprobado " );
+        
+
+        System.out.println("reprobado");
+
+        System.out.println("bueno");
+
+        System.out.println("excelente");
+
+        
+
+
+
+
+
+
+
+
+
+        
+
+     
+
+
+
+
+
     }
+
+    
+        
+               
+        
+    
 }
-// clases pascal case Mi clase primero en mayuscula 
-//Variable/metodo camel case primera en minuscula y luego en mayucula  
-//constante sigue siendo una variable y para saber que es una constante vamos en mayuscula 
+
+
